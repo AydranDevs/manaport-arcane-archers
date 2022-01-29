478c1d954ea150a091deb9ff50d32b57d2a99fac
AydranDevs <71469253+AydranDevs@users.noreply.github.com>
2022-01-28 23:27:16 -0500

    Grid-Based pathfinding almost done!
    so many erroross ;~;

--- Assets/Scripts/utilities/Grid.cs
+++ Assets/Scripts/utilities/Grid.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
 
-public class Grid {
+public class Grid<TGridObject> {
 
     /* 
     This class creates the grid system used for pathfinding.
@@ -19,10 +20,10 @@ public class Grid {
     private float cellSize;
 
     // Creates a multi-dimensional array 
-    private int[,] gridArray;
+    private TGridObject[,] gridArray;
     private TextMesh[,] debugTextArray;
 
-    public Grid(int width, int height, float cellSize, Vector3 origin) {
+    public Grid(int width, int height, float cellSize, Vector3 origin, Func<Grid<TGridObject>, int, int, TGridObject> createGridObject) {
 
         this.width = width;
         this.height = height;
@@ -30,7 +31,7 @@ public class Grid {
         this.origin = origin;
 
         // Sets multi-dimensional array to equal height and width set by Grid creator
-        gridArray = new int[width, height];
+        gridArray = new TGridObject[width, height];
 
         // same thing but for debug purposes.
         debugTextArray = new TextMesh[width, height];
@@ -38,15 +39,13 @@ public class Grid {
         // Cycling through a multi-dimentional array
         for (int x=0; x < gridArray.GetLength(0); x++) {
             for (int y=0; y < gridArray.GetLength(1); y++) {
-                debugTextArray[x,y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 10, Color.white, TextAnchor.MiddleCenter);
+                debugTextArray[x,y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 7f, Color.white, TextAnchor.MiddleCenter);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
         }
         Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
-
-        SetValue(2, 1, 56);
     }
 
     private Vector3 GetWorldPosition(int x, int y) {
@@ -59,7 +58,7 @@ public class Grid {
     }
 
     // These two are used to set the value of a given gridspace
-    public void SetValue(int x, int y, int value) {
+    public void SetValue(int x, int y, TGridObject value) {
         
         // ignore values that are outside of the grid.
         if(x >= 0 && y >= 0 && x < width && y < height) {
@@ -67,23 +66,25 @@ public class Grid {
             debugTextArray[x, y].text = gridArray[x, y].ToString();
         }
     }
-    public void SetValue(Vector3 worldPosition, int value) {
+    public void SetValue(Vector3 worldPosition, TGridObject value) {
         int x, y;
         GetXY(worldPosition, out x, out y);
         SetValue(x, y, value);
     }
 
     // these two are used to get the value of a given gridspace
-    public int GetValue(int x, int y) {
+    public TGridObject GetValue(int x, int y) {
         if(x >= 0 && y >= 0 && x < width && y < height) {
             return gridArray[x, y];
         }else {
-            return 0;
+            return default(TGridObject);
         }
     }
-    public int GetValue(Vector3 worldPosition) {
+    public TGridObject GetValue(Vector3 worldPosition) {
         int x, y;
         GetXY(worldPosition, out x, out y);
         return GetValue(x, y);
     }
+
+
 }
