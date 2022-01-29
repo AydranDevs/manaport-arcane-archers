946a25a00050ea2bdbb3c749eb21c5138f2e05ba
AydranDevs <71469253+AydranDevs@users.noreply.github.com>
2022-01-29 12:14:15 -0500

    Revert "Grid-Based pathfinding almost done!"
    This reverts commit 478c1d954ea150a091deb9ff50d32b57d2a99fac.

--- Assets/Scripts/utilities/Testing.cs
+++ Assets/Scripts/utilities/Testing.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 using CodeMonkey.Utils;
 
 public class Testing : MonoBehaviour {
-
-    private Pathfinding pathfinding;
-
+    
+    private Grid grid;
+    
     private void Start() {
-        pathfinding = new Pathfinding(10, 10);
+        grid = new Grid(30, 30, 1f, new Vector3(151, -4));
     }
 
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
-            pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
-            List<PathNode> path = pathfinding.FindPath(0, 0, x, y);
-            if (path != null) {
-                for (int i=0; i<path.Count - 1; i++) {
-                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, new Vector3(path[i+1].x, path[i+1].y) * 10f + Vector3.one * 5f, Color.Green);
-                }
-            }
+           grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
         }
     }
 }
