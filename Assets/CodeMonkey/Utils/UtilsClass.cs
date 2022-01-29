8c82f37cb193eafdd40aac1186c52ee9779f4fae
AydranDevs <71469253+AydranDevs@users.noreply.github.com>
2022-01-29 12:19:47 -0500

    I broke something
    uh oh

--- Assets/CodeMonkey/Utils/UtilsClass.cs
+++ Assets/CodeMonkey/Utils/UtilsClass.cs
@@ -95,13 +95,13 @@ namespace CodeMonkey.Utils {
         }
 
         // Create Text in the World
-        public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = sortingOrderDefault) {
+        public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), float fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = sortingOrderDefault) {
             if (color == null) color = Color.white;
-            return CreateWorldText(parent, text, localPosition, fontSize, (Color)color, textAnchor, textAlignment, sortingOrder);
+            return CreateWorldText(parent, text, localPosition, fontSize / 100, (Color)color, textAnchor, textAlignment, sortingOrder);
         }
         
         // Create Text in the World
-        public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder) {
+        public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, float fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder) {
             GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
             Transform transform = gameObject.transform;
             transform.SetParent(parent, false);
@@ -110,7 +110,8 @@ namespace CodeMonkey.Utils {
             textMesh.anchor = textAnchor;
             textMesh.alignment = textAlignment;
             textMesh.text = text;
-            textMesh.fontSize = fontSize;
+            textMesh.fontSize = 50;
+            textMesh.characterSize = fontSize;
             textMesh.color = color;
             textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
             return textMesh;
